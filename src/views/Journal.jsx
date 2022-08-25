4cf14d799b811eaee3050db00a1b6a49820d24e1
zaki <abdullah.muzaki@alodokter.com>
2022-08-25 11:51:57 +0700

    image slide in journal

--- src/views/Journal.jsx
+++ src/views/Journal.jsx
@@ -53,9 +53,15 @@ function Journal() {
   const left = data.filter((_, i)=> (i % 2 === 0))
   const right = data.filter((_, i)=> (i % 2 !== 0))
 
+  const lorem = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33'
+
+
   return (
     <div className='container journal' style={{ background: 'white', alignItems: 'flex-start' }} >
-      <div style={{ width: '30%' }} className="bemper-kiri" ></div>
+      <div className="bemper-kiri" >
+        <img src='https://res.cloudinary.com/dm9ufmxnq/image/upload/v1661264894/sdsd_i0f3hi.webp' alt="" />
+        <p>{lorem}</p>
+      </div>
       <div className='big-table' >
         <div className="left" >
           {
