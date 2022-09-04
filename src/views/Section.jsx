bfe297e5ee4948a35ddf3c3ba9e9ceb3db2b058e
abdullah al muzaki <aamuzakii@gmail.com>
2022-09-04 21:13:31 +0700

    classToReadableTitleDict

--- src/views/Section.jsx
+++ src/views/Section.jsx
@@ -4,11 +4,9 @@ import { Link } from "react-router-dom";
 import { getDatabase, ref, onValue } from "firebase/database";
 import { app } from "../firebase"
 
-
 function Section({ dasar, dasarDict, code }) {
 
-  const [projects, setProjects] = useState(new Array(25).fill({}));
-
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     const database = getDatabase(app);
@@ -46,14 +44,14 @@ function Section({ dasar, dasarDict, code }) {
       layoutMode: 'fitRows',
     })
     return () => isotope.current.destroy()
-  }, [projects])
+  }, [projects]) // harus tambah dependency ternyata buat solve bug
 
   // handling filter key change
   React.useEffect(() => {
     filterKey === '*'
       ? isotope.current.arrange({filter: `*`})
       : isotope.current.arrange({filter: `.${filterKey}`})
-  }, [filterKey, projects])
+  }, [filterKey, projects]) // harus tambah dependency ternyata buat solve bug
 
   const colorDict = {
     'white': 'white',
@@ -64,27 +62,41 @@ function Section({ dasar, dasarDict, code }) {
     setFilterKey(()=> dasarDict[dasar])
   }, [dasar])
 
+  const classToReadableTitleDict = {
+    bogor: "Bogor",
+    depok: "Depok",
+    serang: "Serang",
+    madiun: "Madiun",
+    serang: "Serang",
+    cilegon: "Cilegon",
+    serang: "Serang",
+    y2021: "2021",
+    y2022: "2022"
+  }
+
+  let sectionTitle = classToReadableTitleDict[dasarDict[dasar]]
+
   let c = `satu-row ${container}`
 
   return (
     <div className='section' >
-      <h1 className='font-anu' style={{ textAlign: 'center' }} >{dasarDict[dasar]}</h1>
-    <div className={c} >
-      {
+      <h1 className='font-anu' style={{ textAlign: 'center' }} >{sectionTitle}</h1>
+      <div className={c} >
+        {
           projects.map(({ color, year, program, status, location, name, main_picture }, i) => {
           let kelas = `${item} ${program} ${status} y${year} ${location}`
           return (
-            <div key={i} className={kelas} >
-              <Link to={i.toString()} className='box image' style={{ background: color, color: colorDict[color] }} >
-                <p className='tulisan-kecil-corner small-text' >{name}</p>
-                <div className='box-gradient-layer' ></div>
-                <img className='inner-img' src={main_picture} alt="" />
-              </Link>
-            </div>
-          )
-        })
-      }
-    </div>
+              <div key={i} className={kelas} >
+                <Link to={i.toString()} className='box image' style={{ background: color, color: colorDict[color] }} >
+                  <p className='small-text' >{name}</p>
+                  <div className='box-gradient-layer' ></div>
+                  <img className='inner-img' src={main_picture} alt="" />
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
