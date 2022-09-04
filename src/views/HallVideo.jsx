faaa4149c7d3d2df51b54879828c987011409159
abdullah al muzaki <aamuzakii@gmail.com>
2022-09-04 21:33:40 +0700

    MVP video.addEventListener

--- src/views/HallVideo.jsx
+++ src/views/HallVideo.jsx
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import v1 from '../assets/videos/cmg_3.mp4'
-import v2 from '../assets/videos/madiun_day_night.mp4'
+import v1 from '../assets/videos/cgn_1.mp4'
+import v2 from '../assets/videos/dpk_2.mp4'
+import v3 from '../assets/videos/cmg_3.mp4'
+import v4 from '../assets/videos/madiun_day_night.mp4'
 
 function HallVideo() {
   // Note: Chromium browsers do not allow autoplay in most cases. However, muted autoplay is always allowed.
   const [videoSrc, setVideoSrc] = useState('')
+  const [videoIndex, setVideoIndex] = useState(1)
 
-  let arrOfVideo = [
-    
-  ]
+  let arrOfVideo = [v1, v2, v3, v4]
 
   useEffect(() => {
     let video = document.getElementById('root_video')
@@ -17,6 +18,18 @@ function HallVideo() {
       source.setAttribute('src', v2);
       video.load();
       video.play();
+ 
+      video.addEventListener('ended', function(){
+        source.setAttribute('src', v3);
+        video.load();
+        video.play();
+
+        video.addEventListener('ended', function(){
+          source.setAttribute('src', v4);
+          video.load();
+          video.play();
+        });
+      });
     });
   }, [])
   
