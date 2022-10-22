9115a3c9b9eb5461c3d75e8f8c784abe18fa325b
Muzaki 联想 <abdullah.muzaki@aruna.id>
2022-10-22 10:04:25 +0700

    isolate useZepto

---
+++ src/helper/zepto.js
@@ -0,0 +1,10 @@
+const img1 = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1666271841/EXT_q37luw.webp'
+const img2 = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1666271840/EXT_2_tl1u2x.webp'
+const img3 = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1666271841/EKSTERIOR_3_osjfxg.webp'
+const kafi = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1666357683/serasa/kafi_tunt2r.jpg'
+const muzammil = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1666357887/serasa/muza_zw5l0v.jpg'
+const andra = 'https://manual.co.id/wp-content/uploads/2016/02/Andra-Matin-Interview-15.jpg'
+const gub = 'https://kontraktorsyariah.com/wp-content/uploads/2019/03/60d4a696e54874696919268053484836-575x800.jpg'
+const budi = 'https://ladrianarchitects.files.wordpress.com/2017/01/bp.jpg?w=662'
+
+export const arrOfPhoto = [andra, img2, budi, muzammil, img1, gub, img3, img1, budi, img2, img3, kafi]
