d7793f4c6bc07d85acce99b7747601215c478030
Muzaki新联想 <aamuzakii@gmail.com>
2022-12-20 13:05:10 +0700

    Add contributor

--- src/views/Team2.jsx
+++ src/views/Team2.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { useQuery } from '@apollo/client'
-import { GET_ALL_TEAMS, GET_IMG_BY_ID } from '../graphql/queries'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
-import { getRichTextEntityLinks } from '@contentful/rich-text-links'
-import { Markup } from 'interweave'
+import React, {useState, useEffect} from 'react'
+import {useQuery} from '@apollo/client'
+import {GET_ALL_TEAMS, GET_IMG_BY_ID} from '../graphql/queries'
+import {documentToHtmlString} from '@contentful/rich-text-html-renderer'
+import {getRichTextEntityLinks} from '@contentful/rich-text-links'
+import {Markup} from 'interweave'
 import middleware from '../helper/middleware'
 import style from './Team2.module.scss'
 
 function About() {
-  let { data } = useQuery(GET_ALL_TEAMS, {
-  })
+  let {data} = useQuery(GET_ALL_TEAMS, {})
 
   const [staffs, setStaffs] = useState('')
+  const [contributors, setContributors] = useState('')
   const [boardMembers, setBoardMembers] = useState('')
 
   useEffect(() => {
@@ -25,14 +25,17 @@ function About() {
     if (data) {
       let arr = data.teamCollection.items
 
-      let s = arr.filter(x => x.type === 'staff');
-      let b = arr.filter(x => x.type === 'board member');
+      let s = arr.filter((x) => x.type === 'team')
+      let b = arr.filter((x) => x.type === 'board member')
+      let c = arr.filter((x) => x.type === 'contributor')
 
       b = b.slice().sort((a, b) => a.order - b.order)
       s = s.slice().sort((a, b) => a.order - b.order)
+      c = c.slice().sort((a, b) => a.order - b.order)
 
       setStaffs(s)
       setBoardMembers(b)
+      setContributors(c)
     }
   }, [data])
 
@@ -40,32 +43,43 @@ function About() {
   if (!staffs || !boardMembers) return <></>
 
   return (
-    <div className={style.container} >
-      <h1 className={style.center} >Board Member</h1>
-      <section className={style.board_member} >
-        {
-          boardMembers.map(({ name, bio, photo, position }) => {
-            return <div className={style.single_person} >
+    <div className={style.container}>
+      <h1 className={style.center}>Board Member</h1>
+      <section className={style.board_member}>
+        {boardMembers.map(({name, bio, photo, position}) => {
+          return (
+            <div className={style.single_person}>
               <img src={photo.url} alt="" width={300} />
-              <p className={style.name} >{name}</p>
-              <i className={style.position} >{position}</i>
-              <p dangerouslySetInnerHTML={{ __html: documentToHtmlString(bio.json) }} className={style.injected_rich_text}></p>
+              <p className={style.name}>{name}</p>
+              <i className={style.position}>{position}</i>
+              <p dangerouslySetInnerHTML={{__html: documentToHtmlString(bio.json)}} className={style.injected_rich_text}></p>
             </div>
-          })
-        }
+          )
+        })}
       </section>
-      <h1 className={style.center} >Staff</h1>
-      <section className={style.board_member} >
-        {
-          staffs.map(({ name, bio, photo, position }) => {
-            return <div className={style.single_person} >
+      <h1 className={style.center}>Teams</h1>
+      <section className={style.board_member}>
+        {staffs.map(({name, bio, photo, position}) => {
+          return (
+            <div className={style.single_person}>
               <img src={photo.url} alt="" width={220} />
-              <p className={style.name} >{name}</p>
-              <i className={style.position} >{position}</i>
-              <p dangerouslySetInnerHTML={{ __html: documentToHtmlString(bio.json) }} className={style.injected_rich_text}></p>
+              <p className={style.name}>{name}</p>
+              <i className={style.position}>{position}</i>
+              <p dangerouslySetInnerHTML={{__html: documentToHtmlString(bio.json)}} className={style.injected_rich_text}></p>
             </div>
-          })
-        }
+          )
+        })}
+      </section>
+      <h1 className={style.center}>Contributors</h1>
+      <section className={style.board_member}>
+        {contributors.map(({name, position}) => {
+          return (
+            <div className={style.single_person}>
+              <p className={style.name}>{name}</p>
+              <i className={style.position}>{position}</i>
+            </div>
+          )
+        })}
       </section>
     </div>
   )
