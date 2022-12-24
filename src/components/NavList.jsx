3219f3febd8b6ab192e98f5f433337992b31fc00
Muzaki新联想 <aamuzakii@gmail.com>
2022-12-24 20:27:35 +0700

    disable hover scale in mobile

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import logo from '../assets/impossible_logo.png'
 
 function NavList() {
-
   let arr = [
     {
       name: 'Projects',
@@ -20,17 +19,15 @@ function NavList() {
   ]
 
   return (
-    <nav className="nav-container" >
-      <div style={{ display: 'flex', width: '100%' }} className="inner-nav-container">
+    <nav className="nav-container">
+      <div style={{display: 'flex', width: '100%'}} className="inner-nav-container">
         <NavLink to={''} className="nav-logo">
-          {document.body.clientWidth > 768 ? <img src={logo} alt="" height={30} /> : <img src={logo} alt="" height={20} />}
+          {document.body.clientWidth > 768 ? <img src={logo} alt="" height={30} /> : <img src={logo} alt="" height={21} />}
         </NavLink>
         <ul className="right">
-          {arr.map(({ name }) => (
+          {arr.map(({name}) => (
             <li key={name}>
-              <NavLink to={name.toLowerCase()} >
-                {name}
-              </NavLink>
+              <NavLink to={name.toLowerCase()}>{name}</NavLink>
             </li>
           ))}
         </ul>
