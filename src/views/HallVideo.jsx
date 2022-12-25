3a3c17c0b38f3b3372fa96755fe21d26aa452a00
Muzaki新联想 <aamuzakii@gmail.com>
2022-12-25 17:22:09 +0700

    POC animation

--- src/views/HallVideo.jsx
+++ src/views/HallVideo.jsx
@@ -2,10 +2,14 @@ import React, {useEffect, useRef} from 'react'
 import {useMediaQuery} from 'react-responsive'
 import middleware from '../helper/middleware'
 
-let v1 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1664243857/serasa/videos/1_cgn_mmcsor.mp4'
-let v2 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1664232845/serasa/videos/2_dpk_o1kcvg.mp4'
-let v3 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1664243865/serasa/videos/3_cmg_e4lilc.mp4'
-let v4 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1664243855/serasa/videos/madiun_dn_drwvzq.mp4'
+let v1 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1671956642/serasa/videos/1_yutceh.mp4'
+let v2 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1671956568/serasa/videos/2_zfmicp.mp4'
+let v3 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1671956634/serasa/videos/3_zgxwzz.mp4'
+let v4 = 'https://res.cloudinary.com/dm9ufmxnq/image/upload/v1671956580/serasa/videos/4_vblhg4.jpg'
+let v5 = 'https://res.cloudinary.com/dm9ufmxnq/video/upload/v1671956584/serasa/videos/5_dba5tq.mp4'
+
+v1 = v5
+v2 = v5
 
 function HallVideo() {
   // Note: Chromium browsers do not allow autoplay in most cases. However, muted autoplay is always allowed.
@@ -26,7 +30,7 @@ function HallVideo() {
 
   useEffect(() => {
     var i = 0
-    var txt = 'Lorem ipsum dummy text blabla.'
+    var txt = 'Bring The Nature Get Closer in Your Home'
     var speed = 80
 
     function typeWriter() {
@@ -35,6 +39,7 @@ function HallVideo() {
         i++
         setTimeout(typeWriter, speed)
       }
+      document.getElementById('demo').style.transform = 'scale(0.7)'
     }
     typeWriter()
 
@@ -81,7 +86,7 @@ function HallVideo() {
 
       video.addEventListener('ended', playV2)
       video.removeEventListener('ended', playV1)
-      getVerticalAlignment('-300px', '0')
+      getVerticalAlignment('100px', '0')
     }
 
     video.addEventListener('ended', playV2)
