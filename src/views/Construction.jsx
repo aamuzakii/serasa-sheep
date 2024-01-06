ad018b46c49adaa59b9ab3d758509265739795c1
Muzaki新联想 <aamuzakii@gmail.com>
2024-01-07 06:29:46 +0700

    use json file

--- src/views/Construction.jsx
+++ src/views/Construction.jsx
@@ -10,6 +10,7 @@ import middleware from '../helper/middleware'
 import {GET_BUILT_PROJECTS} from '../graphql/queries'
 import Image from '../components/Image'
 import Manual from '../components/Manual'
+import {txtdata, constAndFee} from './construction'
 
 const firstId = '6aI6XUHSSfJE0w3lb1VWYF'
 
@@ -49,59 +50,6 @@ function Journal() {
   }, [data])
 
   if (!image) return <></>
-
-  const txtdata = [
-    {
-      altText: 'image',
-      description: 'Keluar UANG TERLALU BANYAK saat bangun rumah alias BONCOS',
-      imageSrc: '../../icon/pig.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Pengerjaan oleh tukang yang ASAL dan kurang rapi',
-      imageSrc: '../../icon/broken.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Manipulasi spek bangunan oleh mandoor / kontraktor, kualitas tidak sesuai dengan yang dibayarkan',
-      imageSrc: '../../icon/eat.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Ditinggal KABUR oleh tukang dan tidak ada GARANSI',
-      imageSrc: '../../icon/thief.webp',
-    },
-  ]
-  const constAndFee = [
-    {
-      altText: 'image',
-      imageSrc: 'https://lh3.googleusercontent.com/keep-bbsk/ALhRneHiWolFxJA6ClWQBqMvvGKQsnxzOY-Yq_LqsETsJfYu7hVHkZSmDrj1hXiTJ9FDQR0RTKIG_d0K84RxdfKaG8nprODjjALS-fejqHidg5jq8Zw=s700',
-      description: 'Laporan penggunaan dana klien oleh kami dengan sangat transparan, disertai dengan  nota pembelanjaan & laporan absensi pekerja harian',
-      imageSrc: '../../icon/dollar.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Supplier material yang kami pakai adalah supplier TERMURAH, KW 1 dan TERJAMIN KUALITASNYA',
-      imageSrc: '../../icon/cart.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Tukang yang bekerja dengan kami seluruhnya bersertifikat, dengan track record pekerjaan yang jelas serta lulus berbagai pelatihan',
-      imageSrc: '../../icon/eng_blind.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Terdapat Mandoor profesional yang selalu mengawasi keseluruhan proyek dan memonitor progressnya setiap hari',
-      imageSrc: '../../icon/eng_eye.webp',
-    },
-    {
-      altText: 'image',
-      description: 'Hasil pekerjaan seluruhnya ter GARANSI oleh kami, masa pemeliharaan selama 60 hari setelah pekerjaan selesai',
-      imageSrc: '../../icon/hand.webp',
-    },
-  ]
-
-  const firstSectionImage = 'https://images.ctfassets.net/0wvobgztd3t0/4WmB85OMNMPPK5FvyeaMih/571afb67a5119edb060ffeae98394c3d/CGN90_01.jpg'
 
   return (
     <div className={style.container}>
